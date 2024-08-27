5283b867b8092611e9d469b37c09194092020b21
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-08-27 20:02:23 +0300

    EDT-413/Feat:added styles and substyles class in js script

--- script/main.js
+++ script/main.js
@@ -5,24 +5,40 @@
     popovers.forEach(popover => {
         popover.addEventListener('click', function() {
             const isOpenPopup = popover.classList.contains('popover-open');
-            console.log(isOpenPopup);
+    
             if (!isOpenPopup) {
                 popover.classList.add('popover-open');
-                let title = document.createElement('div');
-                let text = document.createElement('div');
-                let container = document.createElement('div');
+                const title = document.createElement('div');
+                const text = document.createElement('div');
+                const container = document.createElement('div');
                 const content = this.getAttribute('data-content');
                 const header = this.getAttribute('data-title');
-                container.className = 'wrapper';
-                text.className = 'popover-content';
-                text.innerHTML = content;
-                title.className = 'popover-title';
-                title.innerHTML = header;
+                const popoverArrow = document.createElement('div');
+                
+                if (!content && !header) return null;
+                if (content ) {
+                    text.className = 'popover-content';
+                    text.innerHTML = content;
+                    container.prepend(text);
+                };
+
+                if (header ) {
+                    title.className = 'popover-title';
+                    title.innerHTML = header;
+                    container.prepend(title);
+                };
+                
+                if (popoverArrow) {
+                    popoverArrow.className = 'popover-arrow';
+                    container.prepend(popoverArrow);
+                };
+
+                container.className = 'popover-wrapper';
                 popover.prepend(container);
-                container.prepend(text);
-                container.prepend(title);
             } else {
-                const container = popover.querySelector('.wrapper');
+                const container = popover.querySelector('.popover-wrapper');
+                if (!container) return null;
+
                 container.remove();
                 popover.classList.remove('popover-open');
             }
