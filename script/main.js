09688f1d95c6dca3849b03b116003f188b22940a
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-08-27 10:18:05 +0300

    EDT-413/Feat:rewrote the script

--- script/main.js
+++ script/main.js
@@ -1,18 +1,29 @@
-let popup = document.querySelector('.popup');
-let popupForm = document.querySelector('.popup_form');
-let openPopupButtons = document.querySelectorAll('.open-popup');
-let closePopupButton = document.querySelector('.close-popup');
 
-openPopupButtons.forEach((button) => {
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        popup.classList.add('active');
-        popupForm.classList.add('active');
+(function() {
+    const popovers = document.querySelectorAll(`[data-toggle="popover"]`);
+    if (!popovers || popovers.length === 0) return null;
+    popovers.forEach(popover => {
+        popover.addEventListener('click', function() {
+            const isOpenPopup = popover.classList.contains('popover-open');
+            console.log(isOpenPopup);
+            if (!isOpenPopup) {
+                popover.classList.add('popover-open');
+                let title = document.createElement('div');
+                let newDiv = document.createElement('div');
+                newDiv.className = 'dropdown-content';
+                newDiv.innerHTML = 'Some content inside the popover';
+                title.className = 'dropdown-title';
+                title.innerHTML = 'Popover Header';
+                popover.prepend(newDiv);
+                popover.prepend(title);
+                const content = this.getAttribute('data-content');
+            } else {
+                const newDiv = popover.querySelector('.dropdown-content');
+                const title = popover.querySelector('.dropdown-title');
+                newDiv.remove();
+                title.remove();
+                popover.classList.remove('popover-open');
+            }
+        })
     })
-
-});
-
-closePopupButton.addEventListener('click',() =>{
-    popup.classList.remove('active');
-    popupForm.classList.remove('active');
-})
+})();
