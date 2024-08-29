f9d7c7870cc55a59947b247ab7fbce26678ce9e6
Anastasiia-Rechevska <volokh.qs@gmail.com>
2024-08-29 16:06:00 +0300

    EDT-413/Feat:added createElement to a separate function

--- script/main.js
+++ script/main.js
@@ -1,47 +1,53 @@
+(function () {
+  const popovers = document.querySelectorAll(`[data-toggle="popover"]`);
+  if (!popovers || popovers.length === 0) return null;
 
-(function() {
-    const popovers = document.querySelectorAll(`[data-toggle="popover"]`);
-    if (!popovers || popovers.length === 0) return null;
-    popovers.forEach(popover => {
-        popover.addEventListener('click', function() {
-            const isOpenPopup = popover.classList.contains('popover-open');
-    
-            if (!isOpenPopup) {
-                popover.classList.add('popover-open');
-                const title = document.createElement('div');
-                const text = document.createElement('div');
-                const container = document.createElement('div');
-                const content = this.getAttribute('data-content');
-                const header = this.getAttribute('data-title');
-                const popoverArrow = document.createElement('div');
-                
-                if (!content && !header) return null;
-                if (content ) {
-                    text.className = 'popover-content';
-                    text.innerHTML = content;
-                    container.prepend(text);
-                };
+  popovers.forEach((popover) => {
+    popover.addEventListener("click", function () {
+      const isOpenPopup = popover.classList.contains("popover-open");
 
-                if (header ) {
-                    title.className = 'popover-title';
-                    title.innerHTML = header;
-                    container.prepend(title);
-                };
-                
-                if (popoverArrow) {
-                    popoverArrow.className = 'popover-arrow';
-                    container.prepend(popoverArrow);
-                };
+      if (!isOpenPopup) {
+        addElement(popover);
+      } else {
+        const container = popover.querySelector(".popover-wrapper");
+        if (!container) return null;
 
-                container.className = 'popover-wrapper';
-                popover.prepend(container);
-            } else {
-                const container = popover.querySelector('.popover-wrapper');
-                if (!container) return null;
+        container.remove();
+        popover.classList.remove("popover-open");
+      }
+    });
+  });
 
-                container.remove();
-                popover.classList.remove('popover-open');
-            }
-        })
-    })
+  function addElement(popover) {
+    popover.classList.add("popover-open");
+    const content = popover.getAttribute("data-content");
+    const header = popover.getAttribute("data-title");
+    const title = document.createElement("div");
+    const text = document.createElement("div");
+    const container = document.createElement("div");
+    const popoverArrow = document.createElement("div");
+
+    if (!content && !header) return null;
+
+    if (content) {
+      text.className = "popover-content";
+      text.innerHTML = content;
+      container.prepend(text);
+      console.log(content);
+    }
+
+    if (header) {
+      title.className = "popover-title";
+      title.innerHTML = header;
+      container.prepend(title);
+    }
+
+    if (popoverArrow) {
+      popoverArrow.className = "popover-arrow";
+      container.prepend(popoverArrow);
+    }
+    container.className = "popover-wrapper";
+    popover.prepend(container);
+  }
+
 })();
